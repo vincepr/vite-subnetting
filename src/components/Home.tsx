20cd07e14b4c695d747818b5d531e363a5887ec7
vincepr <vincepr@users.noreply.github.com>
2023-02-13 20:05:49 +0000

    deploy: d536b69d4e6e343d3f3658c98d66056aa399e2d6

--- src/components/Home.tsx
+++
@@ -1,8 +0,0 @@
-export default function Home(){
-    return <div>
-        <h1></h1>
-        <p> lorem ipsum blah blah blah</p>
-        <h1>Number Converter</h1>
-        <p> more lorem ipsum blah blah blah....</p>
-    </div>
-}
