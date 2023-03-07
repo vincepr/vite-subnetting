88f95ef5ef65049f6cf6ba053528857a75a9652c
vincepr <vincepr@gmx.de>
2023-03-07 17:26:11 +0100

    did we fix pathing on gh-pages

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,31 @@
 import './App.css'
-import { Link, Route, Switch } from "wouter";
+import { Link, Route, Router, Switch } from "wouter";
 import Home from './components/Home';
 import Converter from './components/Converter';
 import RngGamePage from './components/RngGame';
 import IpGamePage from './components/SubnetGame';
 
 
-// Routing of the "pages" of the Single-Page-Application using wouter to keep dependencies low
+// Routing of the "pages" --- using wouter to keep dependencies/size low
 const App = () => (
   <div className="App">
     <nav>
       <div className="menu">
-        <Link href="/">Home</Link>
-        <Link href="/converter">Binary-Hexa-Decimal</Link>
-        <Link href="/rng-game">Rng-Game</Link>
-        <Link href="/ip-game">Subnetting-Trainer</Link>
+        <Link href="/vite-subnetting/">Home</Link>
+        <Link href="/vite-subnetting/converter">Binary-Hexa-Decimal</Link>
+        <Link href="/vite-subnetting/rng-game">Rng-Game</Link>
+        <Link href="/vite-subnetting/ip-game">Subnetting-Trainer</Link>
       </div>
     </nav>
-    <Switch>        
-      <Route path="/"><Home/></Route>
-      <Route path="/converter"><Converter/></Route>
-      <Route path="/rng-game"><RngGamePage/></Route>
-      <Route path="/ip-game"><IpGamePage/></Route>
-    </Switch>
+    <Router base="/vite-subnetting">
+      <Switch >        
+        <Route path="/"><Home/></Route>
+        <Route path="/converter"><Converter/></Route>
+        <Route path="/rng-game"><RngGamePage/></Route>
+        <Route path="/ip-game"><IpGamePage/></Route>
+        <Route><h2>404, Not Found!</h2></Route>
+      </Switch>
+    </Router>
   </div>
   )
 
