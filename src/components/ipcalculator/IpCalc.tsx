3a0416069437e6761bd75bce35deeea35117ad61
vincepr <vincepr@gmx.de>
2023-03-08 16:32:42 +0100

    working

--- src/components/ipcalculator/IpCalc.tsx
+++ src/components/ipcalculator/IpCalc.tsx
@@ -1,10 +1,11 @@
 import { useState } from "react"
 
-export default function IpCalc(){
+/**  */
+export default function Ip4Calculator(){
     const [ip ,setIp] = useState("10.0.0.0")
     const [oldCidr ,setOldCidr] = useState<number>(8)
     const [newCidr ,setnewCidr] = useState<number>()
-    const [activeElement, setActiveElement] = useState<string>()      // :todo
+    const [activeElement, setActiveElement] = useState<string>()
     const [countSubnets ,setCountSubnets] = useState<number>()
     const [countHosts ,setCountHosts] = useState<number>()
     const [subnets, setSubnets] = useState<SubnetData[]>([])
@@ -19,19 +20,16 @@ export default function IpCalc(){
         if (str.includes(",")) str=str.replaceAll(",",".") 
         setIp(str)
     }
+
     const handleChangeOldCidr =(ev:React.ChangeEvent<HTMLInputElement>) =>{
         let nr = Number(ev.target.value)
         if (isNaN(nr)) return
         if (nr > 29) nr = 29
         setOldCidr(nr)
         if (newCidr && newCidr>nr){
-            setCountHosts(Math.pow(2,(32-oldCidr)));
-            setCountSubnets(Math.pow(2,(oldCidr-nr)))
+            setCountSubnets(Math.pow(2,(newCidr-nr)))
         }
     }
-
-
-
 
     const handleChangeNewCidr =(ev:React.ChangeEvent<HTMLInputElement>) =>{
         if (ev.target.value===""){
@@ -44,12 +42,13 @@ export default function IpCalc(){
                 if (cidr >30) cidr =30
                 setnewCidr(cidr)
                 if (cidr>oldCidr){
-                    setCountHosts(Math.pow(2,(32-cidr)));
+                    setCountHosts(-2+Math.pow(2,(32-cidr)));
                     setCountSubnets(Math.pow(2,(cidr-oldCidr)))
                 }
             }
          } 
     }
+
     const handleChangeCountSubnets =(ev:React.ChangeEvent<HTMLInputElement>) =>{
         if (ev.target.value===""){
             setnewCidr(undefined); setCountSubnets(undefined); setCountHosts(undefined);
@@ -57,11 +56,26 @@ export default function IpCalc(){
         } else {
             setActiveElement("CountSubnets")
             if (!isNaN(Number(ev.target.value))) {
-                setCountSubnets((Number(ev.target.value)))
+                let subnetNr = Number(ev.target.value)
+                //if (subnetNr >4194304) subnetNr = 4194304
+                setCountSubnets(subnetNr)
+
+                let add = Math.ceil(Math.log2(subnetNr))
+                if (add === 0) add = 1
+                let cidr = add + oldCidr
+                if (cidr >30) {
+                    cidr =30
+                    setCountSubnets(Math.pow(2,(cidr-oldCidr)))
+                }
+                setnewCidr(cidr)
+                if (cidr>oldCidr){
+                    setCountHosts(-2+Math.pow(2,(32-cidr)));
+                }
                 
             }
          } 
     }
+
     const handleChangeCountHosts =(ev:React.ChangeEvent<HTMLInputElement>) =>{
         if (ev.target.value===""){
             setnewCidr(undefined); setCountSubnets(undefined); setCountHosts(undefined);
@@ -69,7 +83,18 @@ export default function IpCalc(){
         } else {
             setActiveElement("CountHosts")
             if (!isNaN(Number(ev.target.value))) {
-                setCountHosts((Number(ev.target.value)))
+                let hostCount = Number(ev.target.value)
+                if (hostCount>2147483646) hostCount= 2147483646
+                setCountHosts(hostCount)
+
+                let add = 31- Math.floor(Math.log2(hostCount+1))
+                let cidr = add
+                setnewCidr(cidr)
+                if (cidr>oldCidr){
+                    setCountSubnets(Math.pow(2,(cidr-oldCidr)))
+                }
+
+                
             }
          } 
     }
@@ -86,17 +111,17 @@ export default function IpCalc(){
         let subs = getSubnetData(ip, oldCidr, newCidr)
         setSubnets(subs)
     }
-    
+
 
     return(
         <div className="SubnetCalculator">
             <h2>IPv4-Subnet Calculator</h2>
-            <p>get the Subnets for selected host-count or CIDR</p>
+            <p>berechne (bis zu 200) Subnetze für gewünschte Parameter:</p>
             <label>
                 Ip:
                 <input type="text" value={ip} onChange={e => handleChangeIp(e)} onKeyDown={handleKeyDown} style={isValidIp?{}:{color:"red"}}/>
                 <> / </>
-                <input type="text" value={oldCidr} onChange={e => handleChangeOldCidr(e)} onKeyDown={handleKeyDown}/>
+                <input type="text" value={oldCidr} onChange={e => handleChangeOldCidr(e)} onKeyDown={handleKeyDown} style={{width:"2rem"}}/>
                 </label>  
 
             <label>neue CIDR:
@@ -104,25 +129,29 @@ export default function IpCalc(){
                     onChange={e => handleChangeNewCidr(e)} 
                     onKeyDown={handleKeyDown} 
                     disabled={isDisabled("NewCidr")}
-                    style={isValidCidr && !isDisabled("NewCidr")     ?{}:{color:"red"}}/>
+                    style={(!isDisabled("NewCidr") && !isValidCidr) ?{color:"red"}: {}}/>
                 </label> 
             <label> Subnetzanzahl: 
                 <input type="text" className="formatInput" value={countSubnets ? countSubnets : "" } 
                     onChange={e => handleChangeCountSubnets(e)} 
                     onKeyDown={handleKeyDown} 
-                    disabled={true}/>
+                    disabled={isDisabled("CountSubnets")}
+                    style={(!isDisabled("CountSubnets") && !isValidCidr) ?{color:"red"}: {}}/>
                 </label> 
             <label> anz. Hosts pro Netz: 
                 <input type="text" className="formatInput" value={countHosts ? countHosts : ""} 
                     onChange={e => handleChangeCountHosts(e)} 
                     onKeyDown={handleKeyDown} 
-                    disabled={true}/>
+                    disabled={isDisabled("CountHosts")}
+                    style={(!isDisabled("CountHosts") && !isValidCidr) ?{color:"red"}: {}}/>
                 </label> 
             <button onClick={submitAnswers}>Calculate</button>
             <DrawSubnets subnets={subnets}/>
         </div>
     )
 }
+
+
 
 function DrawSubnets({subnets:subs}:{subnets:SubnetData[]}){
     if ((subs.length<=0)){
