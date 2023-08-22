ef41eb37b513502b2f7cddf1e719f6aa423cc2d5
vincepr <vinceprinceking@gmail.com>
2023-08-22 10:54:54 +0200

    added mouse-cursor over on-hover to links

--- src/components/Home.tsx
+++ src/components/Home.tsx
@@ -7,24 +7,24 @@ export default function Home(){
         <p>Eine Sammlung an Elementen zum training von Subnetting Aufgaben, a'la IHK-Prüfung
         Und von Subnetting Rechnern zum überprüfen von Gerechneten Aufgaben</p>
 
-        <Link href="/converter/"><h3>Binary-Hexa-Decimal</h3></Link>
+        <Link href="/converter/"><h3 style={{cursor: "pointer"}} >Binary-Hexa-Decimal</h3></Link>
         <label>Beliebiges umrechnung von Binär - Dezimal - Hexadezimal.</label>
 
 
-        <Link href="/calculator-v4/"><h3>Subnet-Calculator IPv4</h3></Link>
+        <Link href="/calculator-v4/"><h3 style={{cursor: "pointer"}}>Subnet-Calculator IPv4</h3></Link>
         <label>Berechnet Subnetze für Ipv4 Adressen.</label>
 
 
-        <Link href="/calculator-v6/"><h3>Subnet-Calculator IPv6</h3></Link>
+        <Link href="/calculator-v6/"><h3 style={{cursor: "pointer"}}>Subnet-Calculator IPv6</h3></Link>
         <label>Berechnet Subnetze für Ipv6 Adressen.</label>
 
 
-        <Link href="/ip-game/"><h3>Subnetting-Trainer</h3></Link>
+        <Link href="/ip-game/"><h3 style={{cursor: "pointer"}}>Subnetting-Trainer</h3></Link>
         <label>Generiert Ipv4 und oder Ipv6 Subnetting-Aufgaben zufällig, mit Lösungen.</label>
         <p>___________________________________</p>
 
 
-        <h3>Quellcode</h3>
+        <h3><i>Quellcode</i></h3>
         <p>Geschrieben in JavaScript / React mit Vite als module-bundler, gehosted auf Github-Pages per Auto-Deployment (Github-Workflow/Action) </p>
         <p>Quellcode zu finden auf: </p>
         <a href="https://github.com/vincepr/vite-subnetting">https://github.com/vincepr/vite-subnetting</a>
